c1a863c954a12e882bbadc8867d3f249b2c8ca54
Khiem <khiemduy90@gmail.com>
2017-10-02 17:56:06 -0500

    cssInject, browserSync, and new gulp task added

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync').create();
 
 gulp.task('default', function() {
   console.log("Yay - Gulp task created!");
@@ -14,17 +16,31 @@ gulp.task('html',function () {
 });
 
 gulp.task('styles',function () {
-  return gulp.src('./app/styles/styles.css')
-  .pipe(postcss([cssvars, nested, autoprefixer]))
+  return gulp.src('./app/assets/styles/styles.css')
+  .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
   .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function(){
-  watch('./app/index.html', function() {
-    gulp.start('html');
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
   });
 
-  watch('./app/styles/**/*.css', function() {
-    gulp.start('styles');
+  watch('./app/index.html', function() {
+    browserSync.reload();
   });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+
+});
+
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+  .pipe(browserSync.stream());
 });
